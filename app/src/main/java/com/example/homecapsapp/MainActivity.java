c57b7757c57c097fa02e0a05301e35d119e25398
leeeeyz77 <102798337+leeeeyz77@users.noreply.github.com>
2023-03-18 17:45:01 +0900

    calltext+webview

--- app/src/main/java/com/example/homecapsapp/MainActivity.java
+++ app/src/main/java/com/example/homecapsapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.homecapsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
