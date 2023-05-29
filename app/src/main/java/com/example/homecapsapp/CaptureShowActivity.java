95d24671f98114009f963176f3fa27c811731fb1
piebin <dalparan19@gmail.com>
2023-05-30 00:53:27 +0900

    파이어베이스에서 이미지 받아오기

--- app/src/main/java/com/example/homecapsapp/CaptureShowActivity.java
+++ app/src/main/java/com/example/homecapsapp/CaptureShowActivity.java
@@ -1,18 +1,22 @@
 package com.example.homecapsapp;
 
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Debug;
-import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
@@ -24,29 +28,85 @@ public class CaptureShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captureshow);
-        load = (ImageView)findViewById(R.id.loadimg);
+        ImageView im1, im2, im3;
+
+        im1 = (ImageView)findViewById(R.id.iv1);
+        im2 = (ImageView)findViewById(R.id.iv2);
+        im3 = (ImageView)findViewById(R.id.iv3);
+
+
+
         //액티비티에서 사용할 이미지 뷰
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReference();
-        StorageReference pathReference = storageReference.child("photo");
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageReference = storage.getReference();
+//        StorageReference pathReference = storageReference.child("gs://homecaps.appspot.com/Photo");
 
-        if(pathReference==null){
-            Toast.makeText(CaptureShowActivity.this, "저장소에 사진이 없습니다.", Toast.LENGTH_SHORT).show();
+        StorageReference listRef = FirebaseStorage.getInstance().getReference().child("Photo");
+        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                int i=0;
+                for(StorageReference item : listResult.getItems()){
+/*                    LinearLayout layout = (LinearLayout) findViewById(R.id.maskImageLayout);
 
-        }else{
-            StorageReference submitProfile = storageReference.child("Photo/IMG_0378.jpeg");
-            submitProfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    Glide.with(CaptureShowActivity.this).load(uri).into(load);
-                    //url로 이미지 받아서 올림
+                    ImageView iv = new ImageView(CaptureShowActivity.this);
+                    iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    layout.addView(iv);*/
+                    i++;
+                    getSupportActionBar().setTitle("item is : "+i);
+
+                    switch (i){
+                        case 1:
+                            item.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()){
+                                        Glide.with(CaptureShowActivity.this).load(task.getResult()).into(im1);
+                                    }
+                                }
+                            });
+                        case 2:
+                            item.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()){
+                                        Glide.with(CaptureShowActivity.this).load(task.getResult()).into(im2);
+                                    }
+                                }
+                            });
+                        case 3:
+                            item.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()){
+                                        Glide.with(CaptureShowActivity.this).load(task.getResult()).into(im3);
+                                    }
+                                }
+                            });
+
+
+
+
+/*                    item.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()){
+                                Glide.with(CaptureShowActivity.this).load(task.getResult()).into(iv);
+                            }else{
+                                Toast.makeText(CaptureShowActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });*/
                 }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
 
-                }
-            });
+
+
+            }
         }
-    }
-}
+    });
+}}
